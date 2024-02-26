f7b6b6d2ccef77137f6742ac28df49788fdb9d04
SergeyTsVL <159886384+SergeyTsVL@users.noreply.github.com>
2024-02-26 09:46:09 +0700

    Add files via upload

---
+++ def.py
@@ -0,0 +1,15 @@
+#def print_params():
+#    name = "Сергей"
+#    age = 40
+#   return name, age
+#result = print_params()
+#print("Функция", result)
+
+
+name = "Сергей"
+age = 40
+def print_params(name, age):
+    return name, age
+result = print_params(name, age)
+print("Функция", result)
+print("Функция", result)
