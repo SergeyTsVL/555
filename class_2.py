0c639d759cdbfc07ecc620e72df85c54823b5ab0
SergeyTsVL <159886384+SergeyTsVL@users.noreply.github.com>
2024-03-10 13:34:23 +0700

    Add files via upload

---
+++ class_2.py
@@ -0,0 +1,13 @@
+class House:
+    def __init__(self):
+        self.numberOfFloors = 0
+    def setNewNumberOfFloors(floors):
+        for numberOfFloors in floors:
+            numberOfFloors += 1
+            print(House.setNewNumberOfFloors)
+            # return self.numberOfFloors
+dom = House()
+print(dom.setNewNumberOfFloors)
+# dom.setNewNumberOfFloors(floors=5)
+# print(House.setNewNumberOfFloors.floors)
+# House.setNewNumberOfFloors(floors=5)
