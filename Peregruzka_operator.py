41f426c86b22dc81ea8e8da9df1d9b0f75d1a990
SergeyTsVL <159886384+SergeyTsVL@users.noreply.github.com>
2024-03-18 19:47:26 +0700

    Add files via upload

--- Peregruzka_operator.py
+++ Peregruzka_operator.py
@@ -3,20 +3,12 @@ class Buiding:
         self.numberOfFloors = Floors
         self.buildingType = Types
         print("целочисленный атрибут этажности  и строковый атрибут", int(self.numberOfFloors), str(self.buildingType))
-
     def __eq__(self, other):
 
-        if self.numberOfFloors == self.buildingType:
-            print("True")
-            return other
 
-        else:
-            print("False")
-            return other
+        return self.numberOfFloors == other.numberOfFloors and self.buildingType == other.buildingType
 
 a = Buiding(5, "j")
-
 print(a.__eq__)
 
-a.__eq__(7)
 
