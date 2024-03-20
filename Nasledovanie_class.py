2c0a5c27a98a5949996baa6f592cac9e3d0534c5
SergeyTsVL <159886384+SergeyTsVL@users.noreply.github.com>
2024-03-20 20:57:36 +0700

    Add files via upload

--- Nasledovanie_class.py
+++ Nasledovanie_class.py
@@ -1,16 +1,18 @@
 class Car:
     price = 1000000
     pover = 105
+
     def horse_powers(self):
         print('Цена авто:', self.pover, self.price)
+
 
 class Nissan(Car):
     price = 5000
     pover = 205
 
 class Kia(Car):
-    price = Nissan.price + 5000
-    pover = Car.pover + 305
+    def horse_powers(self):
+        print('Цена авто:', self.pover + 100, self.price + 200000)
 
 
 a = Car()
@@ -21,3 +23,5 @@ s.horse_powers()
 
 d = Kia()
 d.horse_powers()
+
+
