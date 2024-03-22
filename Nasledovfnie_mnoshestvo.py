cb5d13b04a0a5069affc8687bfeb200400105aac
SergeyTsVL <159886384+SergeyTsVL@users.noreply.github.com>
2024-03-22 23:04:40 +0700

    Add files via upload

--- Nasledovfnie_mnoshestvo.py
+++ Nasledovfnie_mnoshestvo.py
@@ -1,19 +1,24 @@
 class Vehicle:
-    vehicle_type = "none"
+    def __init__(self):
+        self.vehicle_type = "none"
 
 class Car:
-    price = 1000000
-    pover = 105
+    def __init__(self):
+        self.price = 1000000
+        self.pover = 105
 
     def horse_powers(self):
-        print('Цена авто:', self.pover, self.price)
+        return self.pover
 
 class Nissan(Car, Vehicle):
-    vehicle_type = "yes"
-    price = 25252
+    def __init__(self):
+        super().__init__()
+        self.price = 5000
+        self.vehicle_type = 'yes'
+
     def horse_powers(self):
         print('Цена авто:', self.pover + 100, self.price + 100000)
 
 a = Nissan()
 print(a.vehicle_type, a.price)
-print(a.horse_powers)
+a.horse_powers()
