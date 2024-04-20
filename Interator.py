02bda802da1bb24603a9b09e3f1a09fa1189bc68
SergeyTsVL <159886384+SergeyTsVL@users.noreply.github.com>
2024-04-20 10:51:36 +0700

    Add files via upload

--- Interator.py
+++ Interator.py
@@ -1,22 +1,25 @@
-class EvenNumbers:
+class EvenNumbers():
     def __init__(self, start, end):
         self.i, self.start, self.end = start, start, end
 
     def __iter__(self):
-        # self.i, self.start, self.end = 0, 0, 1
         return self
     def __next__(self):
         self.i += 1
-        if self.i % 2 == 0 and self.i > self.start:
-            # print('\n')
+        if self.i % 2 == 0:
             if self.i > self.end:
                 raise StopIteration()
             return self.i
+        else:
+            self.i += 1
+            if self.i % 2 == 0:
+                # print('\n')
+                if self.i > self.end:
+                    raise StopIteration()
+                return self.i
 
 
-
-iter = EvenNumbers
+iter = EvenNumbers(10, 25)
 print(iter)
-for i in iter(100, 150):
+for i in iter:
     print(i)
-print(121 in iter(100, 150))
