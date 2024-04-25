25ff5a82ad95d0bb004e1f03273c72d4a2df1846
SergeyTsVL <159886384+SergeyTsVL@users.noreply.github.com>
2024-04-25 21:50:20 +0700

    Add files via upload

---
+++ Potoky_na_classah.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+import time
+from threading import Thread
+
+
+class Knight(Thread):
+    def __init__(self, name, skills, enemies, *args, **kwargs):
+        super(Knight, self).__init__(*args, **kwargs)
+        self.name = name
+        self.skills = skills
+        self.enemies = enemies
+
+    def run(self):
+        print(f'{self.name}, на нас напали!')
+
+        for skill in range(0, self.enemies, self.skills):
+            self.enemies -= self.skills
+            time.sleep(0.1)
+            print(f'{self.name}, сражается {int(skill / self.skills)} день(дня)'
+                  f'для победы потребуется {int(self.enemies / self.skills)} день(дня), осталось {self.enemies} воинов.')
+        if self.enemies <= 0:
+            print(f'{self.name} одержал победу спустя {int(skill / self.skills)} дней')
+
+
+knight1 = Knight("Sir Lancelot", 10, 100) # Низкий уровень умения
+knight2 = Knight("Sir Galahad", 20, 100) # Высокий уровень умения
+knight1.start()
+knight2.start()
+knight1.join()
+knight2.join()
+print('Все битвы закончились!')
