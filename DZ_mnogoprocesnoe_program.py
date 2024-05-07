b506e74f2741a2949aa4b6a85f6451318aa4c602
SergeyTsVL <159886384+SergeyTsVL@users.noreply.github.com>
2024-05-07 22:46:25 +0700

    Update DZ_mnogoprocesnoe_program.py

--- DZ_mnogoprocesnoe_program.py
+++ DZ_mnogoprocesnoe_program.py
@@ -1,50 +1,41 @@
-import random
-from multiprocessing import Process
-
-summ_operation = []
-products = []
-cash_operation1 = []
-data = {}
+from multiprocessing import Process, Manager
 
 
 class WarehouseManager:
 
-    def __init__(self, product, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.product = product
+    def __init__(self):
+        # super().__init__(*args, **kwargs)
+        self.product = Manager().dict()
 
-    def run(self):
-        global data
-        for j in range(5):
-
-            r = ['receipt', 'shipment']
-            random_indeks = int(random.randrange(len(r)))
-            cash_operation = r[random_indeks]
-            cash_operation1.append(cash_operation)
-            # print(cash_operation1)
-            if cash_operation == 'receipt':
-                t = int(random.randint(0, 500))
-                summ_operation.append(t)
+    def process_request(self, request):
+        product1, action, amuont = request
+        if action == 'receipt':
+            if product1 in self.product:
+                self.product[product1] += amuont
             else:
-                f = (- 1) * int(random.randint(0, 500))
-                summ_operation.append(f)
-            data = dict(zip(products, summ_operation))
-            products.append(self.product)
+                self.product[product1] = amuont
+        elif action == 'shipment':
+            if product1 in self.product and self.product[product1] >= amuont:
+                self.product[product1] -= amuont
 
-        print(cash_operation1, summ_operation)
-        print(data)
-        return
+    def run(self, request):
+        processes = []
+        for i in request:
+            process = Process(target=self.process_request, args=(i, ))
+            processes.append(process)
+            process.start()
+        for j in processes:
+            j.join()
+if __name__ == '__main__':
+    ff = WarehouseManager()
+    requests = [
+        ("product1", "receipt", 100),
+        ("product2", "receipt", 150),
+        ("product1", "shipment", 30),
+        ("product3", "receipt", 200),
+        ("product2", "shipment", 50)
+    ]
 
+    ff.run(requests)
+    print(dict(ff.product))
 
-if __name__ == '__main':
-    thread_1 = Process(target=WarehouseManager.run)
-    # thread_2 = WarehouseManager(product='product2')
-    thread_1.start()
-    # thread_2.start()
-    thread_1.join()
-    # thread_2.join()
-
-# ff = WarehouseManager(product='product1')
-# print(ff.run())
-# ff = WarehouseManager(product='product2')
-# print(ff.run())
