ed8014a8ad84fd5bbe57673f654169571ba3cd90
SergeyTsVL <159886384+SergeyTsVL@users.noreply.github.com>
2024-05-30 12:45:57 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,56 @@
+import requests as rq
+import logging
 
-def test():
-    a = int(input('Переменная а = '))
-    b = int(input('Переменная b = '))
-    print(a, b)
-    return a, b
-pover = test()
-print(pover)
+logger_200 = logging.getLogger('success_responses')
+logger_200.setLevel(logging.INFO)
+main_fh_200 = logging.FileHandler("success_responses.log", 'w', 'utf-8')
+formatter_200 = logging.Formatter('%(message)s')
+main_fh_200.setFormatter(formatter_200)
+logger_200.addHandler(main_fh_200)
+
+logger_not_200 = logging.getLogger('blocked_responses')
+logger_not_200.setLevel(logging.WARNING)
+main_fh_not_200 = logging.FileHandler("blocked_responses.log", 'w', 'utf-8')
+formatter_not_200 = logging.Formatter('%(message)s')
+main_fh_not_200.setFormatter(formatter_not_200)
+logger_not_200.addHandler(main_fh_not_200)
+
+logger_no_response = logging.getLogger('bad_responses')
+logger_no_response.setLevel(logging.ERROR)
+main_fh_no_response = logging.FileHandler("bad_responses.log", 'w', 'utf-8')
+formatter_no_response = logging.Formatter('%(message)s')
+main_fh_no_response.setFormatter(formatter_no_response)
+logger_no_response.addHandler(main_fh_no_response)
 
 
-i = 1
-j = 2
-z = 3
-print(i, j, z)
-def test2(i, j, z):
-    return i, j, z
-pover2 = test2(i=10, j=20, z=30)
-print(pover2)
+sites = ['https://instagram.com', 'https://wikipedia.org', 'https://yahoo.com',
+         'https://yandex.ru', 'https://whatsapp.com', 'https://twitter.com', 'https://amazon.com', 'https://tiktok.com',
+         'https://www.ozon.ru']
 
-import modul_1
-# print(modul_1.result)
 
+def print_primes():
+    for site in sites:
+        try:
+            response_200 = rq.get(site, timeout=1)
+            if response_200.status_code == 200:
+                logger_200.info(f'INFO: {site}, response - {response_200.status_code}')
+        except BaseException:
+            print('logger_200')
+
+        try:
+            response_not_200 = rq.get(site, timeout=1)
+            if response_not_200.status_code != 200:
+                logger_not_200.warning(f'WARNING: {site}, response - {response_not_200.status_code}')
+        except BaseException:
+            print('logger_not_200')
+
+        try:
+            response_no_response = rq.get(site, timeout=1)
+            if response_no_response.status_code == None:
+                logger_no_response.error(f'ERROR: {site}, {response_no_response.status_code}')
+        except BaseException:
+            print(logger_no_response.error(f'ERROR: {site}, NO CONNECTION'))
+
+
+if __name__ == '__main__':
+    print_primes()
