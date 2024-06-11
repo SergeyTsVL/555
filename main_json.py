0643e62b3b16d6f7bb8e3cd7b373ec8f0d413a4e
SergeyTsVL <159886384+SergeyTsVL@users.noreply.github.com>
2024-06-11 11:25:45 +0700

    Add files via upload

--- main_json.py
+++ main_json.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 def employees_rewrite(sort_type):
     with open("employees.json", "r") as sort_file:
@@ -18,24 +19,35 @@ def employees_rewrite(sort_type):
 # объединяем "employees" и отсортированный список
         dd = dict()
         dd['employees'] = dd.setdefault('employees', []) + sorted_list
-# в зависимости от sort_type создаем файл под необходимым именем (сделал для своего удобства)
-    if sort_type == 'firstName':
-        file_name = f'employees_{sort_type}_sorted.json'
-    if sort_type == 'lastName':
-        file_name = f'employees_{sort_type}_sorted.json'
-    if sort_type == 'department':
-        file_name = f'employees_{sort_type}_sorted.json'
-    if sort_type == 'salary':
-        file_name = f'employees_{sort_type}_sorted.json'
+
 # Выводим в созданный файл полученную структуру
     with open(file_name, "w") as sort_file1:
         json_employees_sorted1 = json.dump(dd, sort_file1, indent=4)
         return json_employees_sorted1
 
-sort_type = 'firstName'
+sort_type = 'salary'   # задаем sort_type
+
+# в зависимости от sort_type создаем файл под необходимым именем (сделал для своего удобства) + сверяем с pattern
+if sort_type == 'firstName':
+    pattern = r'[fF][iI][rR][sS][Tt][nN][aA][mM][Ee]'
+    file_name = f'employees_{sort_type}_sorted.json'
+if sort_type == 'lastName':
+    pattern = r'[lL][aA][sS][tT][nN][aA][mM][eE]'
+    file_name = f'employees_{sort_type}_sorted.json'
+if sort_type == 'department':
+    pattern = r'[dD][eE][pP][aA][rR][tT][mM][eE][nN][tT]'
+    file_name = f'employees_{sort_type}_sorted.json'
+if sort_type == 'salary':
+    pattern = r'[sS][aA][lL][aA][rR][yY]'
+    file_name = f'employees_{sort_type}_sorted.json'
+
+# проверяем на сооответствие с регулярными выражениями
+sample = re.findall(pattern, sort_type)
+sample_sort_type = ''.join(sample)
+print(sample_sort_type)
 
 # Добавляем исключение
-if sort_type in ['firstName', 'lastName', 'department', 'salary']:
+if sort_type in sample_sort_type:
     employees_rewrite(sort_type)
 else:
     print(f'ValueError(\'Bad key for sorting\')')
