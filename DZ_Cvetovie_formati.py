e3d36d73add323b7e2a4030727dabff53cd4a1b0
SergeyTsVL <159886384+SergeyTsVL@users.noreply.github.com>
2024-06-21 20:42:05 +0700

    DZ_Cvetovie_formati.py

--- DZ_Cvetovie_formati.py
+++ DZ_Cvetovie_formati.py
@@ -1,7 +1,11 @@
 import cv2
 import numpy as np
+import dload
 
-img = cv2.imread('Kartyncka.jpg')
+image = dload.save("https://static.tildacdn.com/tild3333-6138-4134-b762-643261643637/2024-03-06_14-21-44.png",
+                   overwrite=False)
+
+img = cv2.imread(image)
 
 
 new_img = np.zeros(img.shape, dtype='uint8') # создаем  матрицу с каждым числом 0. Прописываем ее размеры
@@ -13,11 +17,11 @@ img = cv2.Canny(img, 50, 50)
 
 con, hir = cv2.findContours(img, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE) # cv2.RETR_LIST-получаем все доступные контуры
 
-cv2.drawContours(new_img, con[:374], -1, (230, 11, 148), 1)
-cv2.drawContours(new_img, con[374:755], -1, (0, 0, 255), 1)
-cv2.drawContours(new_img, con[755:], -1, (0, 255, 0), 1)
+cv2.drawContours(new_img, con[:325], -1, (230, 11, 148), 1)
+cv2.drawContours(new_img, con[325:642], -1, (0, 0, 255), 1)
+cv2.drawContours(new_img, con[642:], -1, (0, 255, 0), 1)
 
-cv2.imshow('Result', new_img)
+cv2.imshow('Result', new_img[70:700, 50:750])
 cv2.waitKey(0)
 
 
