d5d7e4191f28bb0b2eb39c20ca434b8adac93b9d
SergeyTsVL <159886384+SergeyTsVL@users.noreply.github.com>
2024-06-23 08:44:44 +0700

    DZ_Cvetovie_formati.py

--- DZ_Cvetovie_formati.py
+++ DZ_Cvetovie_formati.py
@@ -2,12 +2,10 @@ import cv2
 import numpy as np
 import dload
 
-image = dload.save("https://static.tildacdn.com/tild3333-6138-4134-b762-643261643637/2024-03-06_14-21-44.png",
+image = dload.save("https://static.tildacdn.com/tild3331-6133-4263-b364-633561333339/color_text.jpg",
                    overwrite=False)
 
 img = cv2.imread(image)
-
-
 new_img = np.zeros(img.shape, dtype='uint8') # создаем  матрицу с каждым числом 0. Прописываем ее размеры
 
 img - cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -17,11 +15,14 @@ img = cv2.Canny(img, 50, 50)
 
 con, hir = cv2.findContours(img, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE) # cv2.RETR_LIST-получаем все доступные контуры
 
-cv2.drawContours(new_img, con[:325], -1, (230, 11, 148), 1)
-cv2.drawContours(new_img, con[325:642], -1, (0, 0, 255), 1)
-cv2.drawContours(new_img, con[642:], -1, (0, 255, 0), 1)
+cv2.drawContours(new_img, con[:382], -1, (230, 11, 148), 1)
+cv2.drawContours(new_img, con[382:773], -1, (0, 0, 255), 1)
+cv2.drawContours(new_img, con[773:], -1, (0, 255, 0), 1)
 
-cv2.imshow('Result', new_img[70:700, 50:750])
+# kernel = np.ones((1, 1), np.uint8)
+# new_img = cv2.dilate(new_img, kernel, iterations=0)
+#
+# new_img = cv2.erode(new_img, kernel, iterations=0)  # из жирных линий делаем тонкие
+
+cv2.imshow('Result', new_img[0:500, 0:750])
 cv2.waitKey(0)
-
-
